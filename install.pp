659b084bf82c389c27b45e7faa1aeaeabea02c27
jeffersonomonteiro <j.monteiro@personare.com.br>
2016-03-04 10:14:59 -0300

    commit

--- install.pp
+++ install.pp
@@ -1,20 +1,31 @@
-include apt 
-apt::ppa {'ppa:webupd8team/sublime-text-3 -y':}
+{'apt-update':
+command => 'apt-get update',
+path => '/bin:/usr/bin',
+timeout => 0
+
+}
+
+include apt
+apt::ppa {'ppa:webupd8team/sublime-text-3 -y':
+before => exec ['apt-update']
+}
 
 package {'git':
 ensure => 'installed',
 }
 
 package {'awscli':
-ensure => 'installed'
+ensure => 'installed',
 }
 
 package {'docker.io':
-ensure => 'installed'
+ensure => 'installed',
 }
-
 
 package {'sublime-text-installer':
-ensure => 'present',
+ensure => 'installed',
+require => exec ['apt-update']
 }
+
+
 
